fc13d989a8d522195a866ea791000b58275a17ad
Ginger de Ridder <ginger_de_ridder@hotmail.com>
2023-12-10 20:55:00 +0800

    st

---
+++ final_short_essay.py
@@ -0,0 +1,74 @@
+short_essay_01 = "Impact of Globalization on International Trade: Discuss how globalization has influenced international trade patterns in the last two decades. Consider factors such as technological advancements, multinational corporations, and changes in trade policies."
+
+short_essay_02 = "Comparative Advantage and Trade Theories: Analyze the relevance of the theory of comparative advantage in today's global economy. How do modern trade theories expand upon or differ from traditional comparative advantage theory?"
+
+short_essay_03 = "Role of International Economic Organizations: Evaluate the role of major international economic organizations like the WTO, IMF, and World Bank in shaping the landscape of international trade. Discuss their contributions and any controversies or challenges they face."
+
+short_essay_04 = "Trade Protectionism versus Free Trade: Compare and contrast the benefits and drawbacks of trade protectionism and free trade. Provide examples of countries or regions where these policies have had significant economic impacts."
+
+short_essay_05 = "Analyzing Trade Protection Measures: Discuss the various types of trade protection measures like tariffs, quotas, and subsidies. Analyze their potential impacts on a country's economy and international trade relations."
+
+short_essay_06 = "Pros and Cons of Trade Protectionism: Evaluate the arguments for and against trade protectionism. How do these measures affect domestic industries, consumers, and the global economy?"
+
+short_essay_07 = "Comparative Case Study: Compare two countries that have implemented trade protection measures. Discuss the reasons for these measures, their implementation, and the resulting economic impacts."
+
+short_essay_08 = "Analyzing Trade Protectionism: Evaluate the main arguments for and against trade protectionism. Discuss how these arguments are influenced by different economic, political, and social factors."
+
+short_essay_09 = "Global Impact of Trade Policies: Examine the global impact of trade protection policies on both developing and developed countries. Consider aspects like market access, economic growth, and international relations."
+
+short_essay_10 = "Case Study Analysis: Choose a specific country and analyze its trade protection policies. Discuss the reasons behind these policies and their effects on the domestic economy and international trade relationships."
+
+short_essay_11 = "Analysis of Different Levels of Economic Integration: Discuss the different levels of economic integration, including free trade areas, customs unions, common markets, and economic unions. Analyze the benefits and challenges associated with each level."
+
+short_essay_12 = "Case Study of a Trading Bloc: Choose a specific trading bloc (e.g., the European Union, NAFTA, ASEAN) and analyze its impact on the economies of member countries. Discuss the successes and challenges the bloc has faced in achieving economic integration."
+
+short_essay_13 = "Economic Integration and Globalization: Evaluate the role of economic integration in the context of globalization. Discuss how economic integration influences trade, investment, and economic policy among member countries and with the rest of the world."
+
+short_essay_14 = "Analyzing Balance of Payments Trends: Discuss the trends in the balance of payments for a specific country over the past decade. Analyze the factors that have influenced these trends and their implications for the country's economy."
+
+short_essay_15 = "Balance of Payments and Economic Policy: Evaluate how the balance of payments can influence a country's economic policy decisions. Discuss the implications of a persistent current account deficit or surplus on a country's fiscal and monetary policies."
+
+short_essay_16 = "Globalization and Balance of Payments: Analyze the impact of globalization on the balance of payments in developing versus developed countries. Discuss the challenges and opportunities that globalization presents for the management of the balance of payments."
+
+short_essay_17 = "Economic Integration and Globalization: Evaluate the role of economic integration in the context of globalization. Discuss how economic integration influences trade, investment, and economic policy among member countries and with the rest of the world."
+
+short_essay_18 = "Analysis of Exchange Rate Systems: Compare and contrast floating and fixed exchange rate systems. Discuss the advantages and disadvantages of each system, and provide examples of countries that use each system."
+
+short_essay_19 = "Impact of Exchange Rate Fluctuations: Analyze the impact of exchange rate fluctuations on a country's trade balance, inflation, and overall economic growth. Provide examples to illustrate how these fluctuations can affect an economy."
+
+short_essay_20 = "Role of Central Banks in Exchange Rate Management: Discuss the role of central banks in managing exchange rates, particularly in the context of a managed floating exchange rate system. How do central banks intervene, and what are the intended outcomes of such interventions?"
+
+short_essay_21 = "Interconnectedness of Sustainable Development Goals (SDGs): Discuss the interconnectedness of the Sustainable Development Goals. How do individual goals contribute to the broader vision of sustainable development? Provide examples to illustrate these connections."
+
+short_essay_22 = "Challenges in Achieving Sustainable Development: Identify and analyze the major challenges faced by countries in achieving sustainable development. Consider both developed and developing nations in your discussion, highlighting any differences in challenges they face."
+
+short_essay_23 = "Role of Economic Policies in Sustainable Development: Evaluate the role of economic policies in promoting sustainable development. What kind of policies can governments implement to balance economic growth with environmental conservation and social equity?"
+
+short_essay_24 = "Critical Evaluation of GDP as a Measure of Development: Critically evaluate GDP as a measure of economic development. Discuss its strengths and limitations, and explore alternative measures that provide a more comprehensive view of development."
+
+short_essay_25 = "Comparative Analysis of Development Indicators: Compare and contrast different indicators of development such as GDP per capita, HDI, and GNI per capita. Discuss how each indicator contributes to our understanding of a country's economic and social progress."
+
+short_essay_26 = "Role of Non-Economic Factors in Measuring Development: Discuss the importance of non-economic factors in measuring development. How do indicators like the Gender Inequality Index and the Environmental Performance Index complement traditional economic measures?"
+
+short_essay_27 = "Analysis of Poverty Traps: Examine the concept of poverty traps and their implications for economic development. Discuss how various factors such as health, education, and access to technology contribute to these traps."
+
+short_essay_28 = "Role of Infrastructure in Development: Analyze the role of infrastructure in economic growth and development. How does inadequate infrastructure act as a barrier, and what strategies can be used to overcome this challenge?"
+
+short_essay_29 = "Impact of Economic Inequality on Development: Explore the relationship between economic inequality and development. Discuss the causes of rising inequality in developing countries and its impact on growth and development."
+
+short_essay_30 = "Trade Liberalization Scenario: Imagine a developing country that has recently implemented trade liberalization policies. Discuss the potential short-term and long-term impacts on its economy, considering factors like domestic industries, employment, and foreign investment."
+
+short_essay_31 = "Privatization Case Study: Analyze the case of a country that has undergone extensive privatization of its state-owned enterprises. Discuss the outcomes of this strategy in terms of economic efficiency, public welfare, and overall economic development."
+
+short_essay_32 = "Foreign Direct Investment (FDI) Impact: Evaluate the role of FDI in the economic development of a low-income country. Consider both the benefits and potential challenges, including the impact on local businesses, employment, and technology transfer."
+
+
+SHORT_ESSAYS =[
+    short_essay_01, short_essay_02, short_essay_03, short_essay_04, short_essay_05,
+    short_essay_06, short_essay_07, short_essay_08, short_essay_09, short_essay_10,
+    short_essay_11, short_essay_12, short_essay_13, short_essay_14, short_essay_15,
+    short_essay_16, short_essay_17, short_essay_18, short_essay_19, short_essay_20,
+    short_essay_21, short_essay_22, short_essay_23, short_essay_24, short_essay_25,
+    short_essay_26, short_essay_27, short_essay_28, short_essay_29, short_essay_30,
+    short_essay_31, short_essay_32
+]
